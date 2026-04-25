fe337d6a8b9edd7a5caee091758204c450e16420
Kevin <kshen3005@gmail.com>
2026-04-24 21:56:08 -0400

    rough pi calibration edit

--- pi/workspace_calibration.py
+++ pi/workspace_calibration.py
@@ -40,10 +40,10 @@ FRAME_HEIGHT      = 720
 
 # World coords (mm) of the 4 fiducials, matched to click order A,B,C,D
 WORLD_POINTS = np.array([
-    [ 18,   0.0],   # A: +X (short side of one of those mini breadboards)
-    [  0.0,  23],   # B: +Y (long side of one of those mini breadboards)
-    [-18,   0.0],   # C: -X (short side of one of those mini breadboards)
-    [  0.0, -23],   # D: -Y (long side of one of those mini breadboards)
+    [ 23,   0.0],   # A: +X (long side of one of those mini breadboards)
+    [  0.0,  18],   # B: +Y (short side of one of those mini breadboards)
+    [-23,   0.0],   # C: -X (long side of one of those mini breadboards)
+    [  0.0, -18],   # D: -Y (short side of one of those mini breadboards)
 ], dtype=np.float32)
 
 clicked_points = []
